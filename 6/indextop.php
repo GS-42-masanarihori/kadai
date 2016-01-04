bf18a8475d19ac069f9b4e33fbc5b805f6998c6f
堀正斉 <masanarihori@gmail.com>
2016-01-04 12:23:56 +0900

    indextop.php

---
+++ 6/indextop.php
@@ -0,0 +1,57 @@
+<?php 
+
+//トップページ
+
+//①HTMLファイルで仮のページを作る（どっかに簡易的なログイン機能あればOK）
+//②ログイン部分に要素が入ったらそれをPOSTで送信する
+//③アンケート部分に進むボタンを押すと、input_enq.phpに飛ばす
+//④ログイン機能をつける。sessionで情報をキープ
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>TOPページ</title>
+	<link rel="stylesheet" href="./css/style.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+</head>
+<body>
+<div class="main-contents">
+	<img src="./img/monkey.png" alt="monkey">
+</div>
+
+<div class="message">
+	<p><?php echo "みなさま"?></p>
+	<p>あけましておめでとうございます。</p>
+	<p>昨年は大変お世話になりました。</p>
+	<p>本年は昨年以上に課題も大変になるかと思いますが</p>
+	<p>ともに乗り越えていきましょう！</p>
+	<p>本年もどうぞ宜しくお願い致します。</p>
+</div>
+
+<div class="login-text">
+	<p>新年の運試し！ということで</p>
+	<p>アンケートに答えておみくじを引こう！</p>
+	<p>↓↓ログインしてアンケートに答える↓↓</p>
+</div>
+
+<div class="login">
+	<form action="input_enq.php" method="post">
+		<dl>
+			<dt>ID</dt>
+			<dd><input type="text" id="my_id" name="my_id"></dd>
+			<dt>パスワード</dt>
+			<dd><input type="password" id="password" name="password"></dd>
+		</dl>
+		<div class="submit">
+			<input type="submit" value="ログインする">
+		</div>
+	</form>
+</div>
+
+
+
+</body>
+</html>
