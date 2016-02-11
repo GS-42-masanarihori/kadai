ac440f9e2ed35012814e20accb2daf6579bd62ea
堀正斉 <masanarihori@gmail.com>
2016-02-11 23:26:16 +0900

    今回から卒業課題の制作に入ります。

---
+++ 10/php/register_do.php
@@ -0,0 +1,17 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<title>登録完了ページ</title>
+	<link rel="stylesheet" href="css/register_do.css">
+</head>
+<body>
+	<div class="register_do">
+		<p>登録完了しました。</p>
+		<a href="../editarea.html">早速作業を始める</a>
+	</div>	
+</body>
+</html>
